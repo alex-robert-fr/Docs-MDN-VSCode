c9f25078456a7cba3c03e51a0ff0df0fbed136c3
Alex Robert <contact.alexrobert04@gmail.com>
2021-08-09 21:23:52 +0200

    Precise search of the selected word on the MDN with a command

--- extension.js
+++ extension.js
@@ -1,35 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate (context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "docs-mdn" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('docs-mdn.docsmdn', () => {
-    // The code you place here will be executed every time your command is executed
+  const globalSearch = vscode.commands.registerCommand('docs-mdn.docsmdnglobal', () => {
     const editor = vscode.window.activeTextEditor
     const selectText = editor.document.getText(editor.selection)
     const url = 'https://developer.mozilla.org/fr/search?q=' + encodeURI(selectText)
     vscode.env.openExternal(vscode.Uri.parse(url))
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Docs MDN, ' + url + ' !')
   })
 
-  context.subscriptions.push(disposable)
+  const preciseSearch = vscode.commands.registerCommand('docs-mdn.docsmdnprecise', () => {
+    const editor = vscode.window.activeTextEditor
+    const selectText = editor.document.getText(editor.selection)
+    const url = 'https://developer.mozilla.org/fr/docs/Web/API/' + selectText.replace('.', '/')
+    vscode.env.openExternal(vscode.Uri.parse(url))
+  })
+
+  context.subscriptions.push(globalSearch, preciseSearch)
 }
 
-// this method is called when your extension is deactivated
 function deactivate () {}
 
 module.exports = {
